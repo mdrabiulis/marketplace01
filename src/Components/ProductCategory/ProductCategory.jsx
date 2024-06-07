45bf7e097212167fa4810af6eac7c77e7fdc147a
mdrabiulis <mdrabiulis155@gmail.com>
2024-06-07 16:54:48 +0600

    react icon add

--- src/Components/ProductCategory/ProductCategory.jsx
+++ src/Components/ProductCategory/ProductCategory.jsx
@@ -1,18 +1,48 @@
-import "./ProductCategory.css"
+import { Link } from "react-router-dom";
+import { GoChevronRight } from "react-icons/go";
+
+import "./ProductCategory.css";
 
 const ProductCategory = () => {
-    return (
-        <div className="pl-40 pt-9">
-            <ul className="">
-                <li>Smartphones</li>
-                <li>Electronics & Appliances</li>
-                <li>Mobile Accessories</li>
-                <li>Computers</li>
-                <li>Computer Accessories</li>
-                <li>Lifestyle</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="pl-40 pt-9">
+      <ul className="">
+        <li>
+          <Link className="flex justify-between pr-8  items-center " to={"/"}>
+            Smartphones <GoChevronRight className="w-6 h-6"></GoChevronRight>
+          </Link>
+        </li>
+        <li>
+          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+            Lifestyle <GoChevronRight className="w-6 h-6"></GoChevronRight>
+          </Link>
+        </li>
+        <li>
+          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+            Computers <GoChevronRight className="w-6 h-6"></GoChevronRight>
+          </Link>
+        </li>
+        <li>
+          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+            Mobile Accessories{" "}
+            <GoChevronRight className="w-6 h-6"></GoChevronRight>
+          </Link>
+        </li>
+        <li>
+          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+            Computer Accessories{" "}
+            <GoChevronRight className="w-6 h-6"></GoChevronRight>
+          </Link>
+        </li>
+        <li>
+          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+            Electronics & Appliances{" "}
+            <GoChevronRight className="w-6 h-6"></GoChevronRight>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default ProductCategory;
