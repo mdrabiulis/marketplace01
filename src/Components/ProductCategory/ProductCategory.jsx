3592b2d237078ff38e234914a720a472f9133591
mdrabiulis <mdrabiulis155@gmail.com>
2024-06-10 01:53:25 +0600

    ProductCategory

--- src/Components/ProductCategory/ProductCategory.jsx
+++ src/Components/ProductCategory/ProductCategory.jsx
@@ -7,35 +7,63 @@ const ProductCategory = () => {
   return (
     <div className="pl-40 pt-9">
       <ul className="">
-        <li>
+        <li className="hover:bg-sky-700 ">
+          <Link className="flex justify-between pr-8  items-center " to={"/"}>
+            Smartphones <GoChevronRight className="w-6 h-6"></GoChevronRight>
+          </Link>
+          <ul className="">
+            <li>a</li>
+            <li>d</li>
+            <li>g</li>
+            <li>h</li>
+            <li>a</li>
+          </ul>
+        </li>
+
+        <li className="">
           <Link className="flex justify-between pr-8  items-center " to={"/"}>
             Smartphones <GoChevronRight className="w-6 h-6"></GoChevronRight>
           </Link>
         </li>
         <li>
-          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+          <Link
+            className="flex justify-between pr-8 items-center gap-24"
+            to={"/"}
+          >
             Lifestyle <GoChevronRight className="w-6 h-6"></GoChevronRight>
           </Link>
         </li>
         <li>
-          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+          <Link
+            className="flex justify-between pr-8 items-center gap-24"
+            to={"/"}
+          >
             Computers <GoChevronRight className="w-6 h-6"></GoChevronRight>
           </Link>
         </li>
         <li>
-          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+          <Link
+            className="flex justify-between pr-8 items-center gap-24"
+            to={"/"}
+          >
             Mobile Accessories{" "}
             <GoChevronRight className="w-6 h-6"></GoChevronRight>
           </Link>
         </li>
         <li>
-          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+          <Link
+            className="flex justify-between pr-8 items-center gap-24"
+            to={"/"}
+          >
             Computer Accessories{" "}
             <GoChevronRight className="w-6 h-6"></GoChevronRight>
           </Link>
         </li>
         <li>
-          <Link className="flex justify-between pr-8 items-center gap-24" to={"/"}>
+          <Link
+            className="flex justify-between pr-8 items-center gap-24"
+            to={"/"}
+          >
             Electronics & Appliances{" "}
             <GoChevronRight className="w-6 h-6"></GoChevronRight>
           </Link>
